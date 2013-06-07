9e05930438bf05dd628fae2fdf079ef1b097c4ba
Steven Degutis <steven@cleancoders.com>
2013-06-07 15:00:48 -0500

    uhhh...

--- src/test2/expect.clj
+++ src/test2/expect.clj
@@ -3,15 +3,44 @@
   (:require [clojure.string :as s]
             [test2.api.asserters :refer [report-error report-pass report-fail]]))
 
-(defn -expect [f args results])
+(defn -expect [f args results]
+  ;; uhh..
+  )
+
+;; (defn- file-and-line ;; TODO: move this somewhere else, for everyone to use
+;;   [exception depth]
+;;   (let [^StackTraceElement s (nth (.getStackTrace exception) depth)]
+;;     {:file (.getFileName s) :line (.getLineNumber s)}))
 
 (defmacro expect
   "Runs (apply f args) and reports on the result."
   [f & args]
-  ;; ...
-  (try
-    (catch Exception e
-      (report-error nil e))))
+
+  ;; `(prn ~(file-and-line (new java.lang.Throwable) 1))
+  ;; (let [result `(~f ~@args)]
+  ;;   (prn result)
+  ;;   `(prn "hi")
+  ;;   ;; `(do
+  ;;   ;;    (prn ~f)
+  ;;   ;;    (prn ~args)
+  ;;   ;;    ;; (report-pass nil)
+  ;;   ;;    ;; (report-fail nil
+  ;;   ;;    ;;              "yes"
+  ;;   ;;    ;;              8
+  ;;   ;;    ;;              'foo
+  ;;   ;;    ;;              ~(name f)
+  ;;   ;;    ;;              '[1 2 3]
+  ;;   ;;    ;;              '[1 2 3]
+  ;;   ;;    ;;              )
+  ;;   ;;    ;; (prn (~f ~@args))
+  ;;   ;;    )
+  ;;   )
+
+  ;; ;; ...
+  ;; (try
+  ;;   (catch Exception e
+  ;;     (report-error nil e)))
+  )
 
 (def ^{:doc "Nicer way of saying identity"}
   truthy? identity)
