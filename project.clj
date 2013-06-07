104d73a33cd1f0e7bd41b3a2da2d7dacd264e63d
Steven Degutis <steven@cleancoders.com>
2013-06-07 02:54:52 -0500

    codox

--- project.clj
+++ project.clj
@@ -7,6 +7,9 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [bultitude "0.2.2"]]
 
+  :codox {:src-dir-uri "https://github.com/evanescence/test2/blob/master"
+          :src-linenum-anchor-prefix "L"}
+
   ;; :plugins [[lein-test2 "0.1.0-SNAPSHOT"]]
   ;; this shouldnt really be in this project.
   ;; im just testing it out.
