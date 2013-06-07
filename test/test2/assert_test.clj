1554a55456f620110e964c04aa8c3f61a633c6e2
Steven Degutis <steven@cleancoders.com>
2013-06-07 16:22:22 -0500

    better test

--- test/test2/assert_test.clj
+++ test/test2/assert_test.clj
@@ -5,22 +5,35 @@
 
 (defn ^:test real-test-1 []
   (expect empty? [])
-  (expect empty? [1 2 (+ 1 2)]))
+  (expect empty? [1 2 (+ 1 2)])
+  (expect empty? (/ 1 0)))
 
 (deftest testing-assert
-  (let [result (run-test-fn #'real-test-1)]
-    (is (= {:test #'real-test-1
-            :results [{:status :pass
-                       :file "assert_test.clj"
-                       :line 7}
-                      {:status :fail
-                       :file "assert_test.clj"
-                       :line 8
-                       :failure-details {:result false
-                                         :fn 'empty?
-                                         :args [[1 2 3]]
-                                         :raw-args '[[1 2 (+ 1 2)]]}}]}
-           result))))
+  (let [test-result (run-test-fn #'real-test-1)
+        assertion-results (:results test-result)]
+    (is (= [:test :results] (keys test-result)))
+    (is (= #'real-test-1
+           (:test test-result)))
+    (is (= 3 (count assertion-results)))
+    (let [[r1 r2 r3] assertion-results]
+      (is (= {:status :pass
+              :file "assert_test.clj"
+              :line 7}
+             r1))
+      (is (= {:status :fail
+              :file "assert_test.clj"
+              :line 8
+              :failure-details {:result false
+                                :fn 'empty?
+                                :args [[1 2 3]]
+                                :raw-args '[[1 2 (+ 1 2)]]}}
+             r2))
+      ;; (is (= {:status :error
+      ;;         :file "assert_test.clj"
+      ;;         :line 7
+      ;;         :exception 4}
+      ;;        r3))
+      )))
 
 (defn test-ns-hook []
   (testing-assert))
