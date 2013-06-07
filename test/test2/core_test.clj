adb80a84038317f5f8d7a2686b1a535d4f650fbe
Steven Degutis <steven@cleancoders.com>
2013-06-07 01:03:14 -0500

    easier to read

--- test/test2/core_test.clj
+++ test/test2/core_test.clj
@@ -5,4 +5,6 @@
 
 (deftest testing-test-fns-in-ns
   (let [fns (#'test2.core/test-fns-in-ns 'test2.fake-test)]
-    (is (= #{#'fake-test-1 #'fake-test-3} (set fns)))))
+    (is (= #{#'fake-test-1
+             #'fake-test-3}
+           (set fns)))))
