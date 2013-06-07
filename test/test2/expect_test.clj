7ca926dd560742618b992ffc1628b810244804f9
Steven Degutis <steven@cleancoders.com>
2013-06-07 10:56:13 -0500

    deftest/defspec

--- test/test2/expect_test.clj
+++ test/test2/expect_test.clj
@@ -9,7 +9,7 @@
   "foo"
   (+ 1 2))
 
-(deftest testing-new-deftest
+(deftest testing-deftest
   (is (= 'fake-deftest-1 (:name (meta #'fake-deftest-1))))
   (is (= nil (:doc (meta #'fake-deftest-1))))
   (is (= 3 (fake-deftest-1)))
@@ -18,5 +18,21 @@
   (is (= "foo" (:doc (meta #'fake-deftest-2))))
   (is (= 3 (fake-deftest-2))))
 
+(p/defspec "really cool test"
+  (+ 1 2))
+
+(p/defspec "it, even has.. bad chars in the name!!!"
+  (+ 1 2))
+
+(deftest testing-defspec
+  (is (= 'really-cool-test (:name (meta #'really-cool-test))))
+  (is (= "really cool test" (:doc (meta #'really-cool-test))))
+  (is (= 3 (really-cool-test)))
+
+  (is (= 'it-even-has-bad-chars-in-the-name (:name (meta #'it-even-has-bad-chars-in-the-name))))
+  (is (= "it, even has.. bad chars in the name!!!" (:doc (meta #'it-even-has-bad-chars-in-the-name))))
+  (is (= 3 (it-even-has-bad-chars-in-the-name))))
+
 (defn test-ns-hook []
-  (testing-new-deftest))
+  (testing-deftest)
+  (testing-defspec))
