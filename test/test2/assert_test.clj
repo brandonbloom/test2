6c0c1d7a7a4a5e0f57311e35ccffcac2e3e6aacf
Steven Degutis <steven@cleancoders.com>
2013-06-07 13:08:53 -0500

    test

---
+++ test/test2/assert_test.clj
@@ -0,0 +1,26 @@
+(ns test2.assert-test
+  (:require [clojure.test :refer :all]
+            [test2.api.runners :refer [run-test-fn]]
+            [test2.expect :refer :all]))
+
+(defn ^:test real-test-1 []
+  (expect empty? [])
+  (expect empty? [1 2 (+ 1 2)]))
+
+(deftest testing-assert
+  (let [result (run-test-fn #'real-test-1)]
+    (is (= {:test #'real-test-1
+            :results [{:status :pass
+                       :description nil}
+                      {:status :fail
+                       :description nil
+                       :failure-report {:file "assert_test.clj"
+                                        :line 8
+                                        :result false
+                                        :fn 'empty?
+                                        :args [1 2 3]
+                                        :raw-args '[1 2 (+ 1 2)]}}]}
+           result))))
+
+(defn test-ns-hook []
+  (testing-assert))
