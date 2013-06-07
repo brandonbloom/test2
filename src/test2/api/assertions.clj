be3b4a97d5c6763a3d39a31651398c6c3b83b9f9
Steven Degutis <steven@cleancoders.com>
2013-06-07 13:08:48 -0500

    workin

--- src/test2/api/assertions.clj
+++ src/test2/api/assertions.clj
@@ -6,17 +6,33 @@
   "Creates an assertion-result appropriate for unexpected errors.
   Conj's the result onto *assertions-results* for you."
   [desc e]
-  :todo)
+  (dosync
+   (alter *assertions-results*
+          conj {:status :error
+                :description desc
+                :error e})))
 
 (defn report-pass
   "Creates an assertion-result appropriate for a passed test.
   Conj's the result onto *assertions-results* for you."
   [desc]
-  :todo)
+  (dosync
+   (alter *assertions-results*
+          conj {:status :pass
+                :description desc})))
 
 (defn report-fail
   "Creates an assertion-result appropriate for a Failed test.
   All params are based on assertion-result in the SPEC.
   Conj's the result onto *assertions-results* for you."
   [desc file line result fn args raw-args]
-  :todo)
+  (dosync
+   (alter *assertions-results*
+          conj {:status :fail
+                :description desc
+                :file file
+                :line line
+                :result result
+                :fn fn
+                :args args
+                :raw-args raw-args})))
