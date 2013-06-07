114b4041a1b5ceb8de9f8745460d4f02370a2263
Steven Degutis <steven@cleancoders.com>
2013-06-07 16:40:53 -0500

    todo

--- src/test2/default/reporter.clj
+++ src/test2/default/reporter.clj
@@ -4,5 +4,17 @@
 (defn default-reporter
   "Prints only failures. Doesn't colorize the text."
   [test-results]
-  nil
+  (prn test-results)
+  ;; (let [failures (group-by :status test-results)]
+  ;;   (doseq [failure failures]
+  ;;     (println (format "FAIL in %s at line %s\n"
+  ;;                      (:file failure)
+  ;;                      (:line failure))
+  ;;              (format " Expected: %s"
+  ;;                      (:file failure)
+  ;;                      (:line failure)))
+
+  ;;     )
+
+  ;;   )
   )
