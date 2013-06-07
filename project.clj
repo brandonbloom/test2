d39b2a88ff98dcebfb2c7cedf97588a8f655019a
Steven Degutis <steven@cleancoders.com>
2013-06-06 23:30:05 -0500

    oh yeah

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject test2 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Composable testing lib for Clojure"
+  :url "https://github.com/evanescence/test2"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.5.1"]])
