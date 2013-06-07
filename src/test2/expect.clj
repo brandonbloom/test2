503483af59e9bf8dec7e3faa5cf04d91381c91d2
Steven Degutis <steven@cleancoders.com>
2013-06-07 13:12:36 -0500

    uhh

--- src/test2/expect.clj
+++ src/test2/expect.clj
@@ -1,12 +1,14 @@
 (ns test2.expect
   (:require [clojure.string :as s]
-            [test2.api.assertions :refer [*assertions-results*]]))
+            [test2.api.assertions :refer [report-error report-pass report-fail]]))
 
 (defmacro expect
   "Runs (apply f args) and reports on the result."
   [f & args]
-  ;; does something with *assertions-results*
-  )
+  ;; ...
+  (try
+    (catch Exception e
+      (report-error nil e))))
 
 (def ^{:doc "Nicer way of saying identity"}
   truthy? identity)
