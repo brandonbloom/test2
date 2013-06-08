a0181befa2ec61c229cb10ce2bb137268a931e04
Steven Degutis <steven@cleancoders.com>
2013-06-07 19:49:48 -0500

    cool

--- src/test2/default/reporter.clj
+++ src/test2/default/reporter.clj
@@ -1,11 +1,24 @@
 (ns test2.default.reporter
   "Some built-in test-reporters.")
 
+(defn exit-with-code [code]
+  (System/exit code))
+
 (defn default-reporter
   "Prints only failures. Doesn't colorize the text."
   [test-results]
-  (if (not (empty? (:fail (group-by :status (mapcat :results test-results)))))
-    (println "FAIL"))
+  (let [failed? (not (empty? (:fail (group-by :status (mapcat :results test-results)))))]
+    (if failed?
+      (println "FAIL"))
+    (println (format "Ran %s tests containing %s assertions.\n%s failures, %s errors."
+                     1
+                     2
+                     3
+                     4
+                     ))
+    (exit-with-code (if failed? 1 0)))
+
+
 
   ;; (let [failures (group-by :status test-results)]
   ;;   (doseq [failure failures]
--- test/test2/run_test.clj
+++ test/test2/run_test.clj
@@ -1,7 +1,8 @@
 (ns test2.run-test
   (:require [clojure.test :refer :all]
             [test2.fake-test]
-            [test2.run]))
+            [test2.run]
+            [test2.default.reporter :refer [exit-with-code]]))
 
 (deftest testing-test-fns-in-ns
   (let [fns (#'test2.run/test-fns-in-ns 'test2.fake-test)]
@@ -10,12 +11,16 @@
            (set fns)))))
 
 (deftest testing-running-high-level
-  (let [s (with-out-str
-            (test2.run/run-tests :namespaces ['test2.failing-test]))]
-    (is (.contains s "FAIL")))
-  (let [s (with-out-str
-            (test2.run/run-tests :namespaces ['test2.passing-test]))]
-    (is (not (.contains s "FAIL")))))
+  (let [exit-code (ref nil)]
+    (with-redefs [exit-with-code #(dosync (ref-set exit-code %))]
+      (let [s (with-out-str
+                (test2.run/run-tests :namespaces ['test2.failing-test]))]
+        (is (.contains s "FAIL"))
+        (is (= @exit-code 1)))
+      (let [s (with-out-str
+                (test2.run/run-tests :namespaces ['test2.passing-test]))]
+        (is (not (.contains s "FAIL")))
+        (is (= @exit-code 0))))))
 
 (defn test-ns-hook []
   (testing-test-fns-in-ns)
