67433dbdc03f022dd58e0ef99ef522924970bec0
Steven Degutis <steven@cleancoders.com>
2013-06-07 21:30:28 -0500

    shhh

--- src/test2/default/reporter.clj
+++ src/test2/default/reporter.clj
@@ -16,29 +16,17 @@
         groups (group-by :status assertion-results)
         failures (:fail groups)
         errors (:error groups)
-        problem? (and (not (empty? failures))
-                      (not (empty? errors)))]
+        problem? (or (not (empty? failures))
+                     (not (empty? errors)))]
     (doseq [failure failures]
-      (println (format "In %s at line %s"
-                       (:file failure)
-                       (:line failure)))
-      (println (format "    FAIL: (%s %s)"
-                       (:fn failure)
-                       (apply str (:raw-args failure))))
-      (println (format "Expected: (%s %s)"
-                       (:fn failure)
-                       (apply str (:args failure))))
-      (println (format "     Got: %s"
-                       (:result failure))))
+      (println (format "In %s at line %s" (:file failure) (:line failure)))
+      (println (format "    FAIL: (%s %s)" (:fn failure) (apply str (:raw-args failure))))
+      (println (format "Expected: (%s %s)" (:fn failure) (apply str (:args failure))))
+      (println (format "     Got: %s" (:result failure))))
     (doseq [error errors]
-      (println (format "In %s at line %s"
-                       (:file error)
-                       (:line error)))
-      (println (format "   ERROR: (%s %s)"
-                       (:fn error)
-                       (apply str (:raw-args error))))
-      (println (format "     Got: %s"
-                       (:exception error))))
+      (println (format "In %s at line %s" (:file error) (:line error)))
+      (println (format "   ERROR: (%s %s)" (:fn error) (apply str (:raw-args error))))
+      (println (format "     Got: %s" (:exception error))))
     (if problem?
       (println "\nTEST FAILED\n"))
     (println (format "Ran %s tests containing %s assertions.\n%s failures, %s errors."
