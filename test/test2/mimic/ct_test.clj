73eb9d170109c8337f0e14a188f9a311c0a08ce6
Steven Degutis <steven@cleancoders.com>
2013-06-08 18:22:31 -0500

    RIP, idea. clojure.test needs a custom runner :(

--- test/test2/mimic/ct_test.clj
+++ test/test2/mimic/ct_test.clj
@@ -2,8 +2,8 @@
   (:require [clojure.test :as ct]
             [test2.mimic.ct-test-data]))
 
-(ct/deftest once-fixtures
-  (ct/is (= "f1 t1 t2 f1"
-            (with-out-str
-              (test2.mimic.ct-test-data/t1)
-              (test2.mimic.ct-test-data/t2)))))
+;; (ct/deftest once-fixtures
+;;   (ct/is (= "f1 t1 t2 f1"
+;;             (with-out-str
+;;               (test2.mimic.ct-test-data/t1)
+;;               (test2.mimic.ct-test-data/t2)))))
--- test/test2/mimic/ct_test_data.clj
+++ test/test2/mimic/ct_test_data.clj
@@ -1,18 +1,18 @@
 (ns test2.mimic.ct-test-data
   (:require [test2.mimic.clojure-test :as ct]))
 
-(ct/deftest t1
-  (print "t1 "))
+;; (ct/deftest t1
+;;   (print "t1 "))
 
-(ct/deftest t2
-  (print "t2 "))
+;; (ct/deftest t2
+;;   (print "t2 "))
 
-(defn f1 []
-  (print "f1 "))
+;; (defn f1 []
+;;   (print "f1 "))
 
-(defn f2 []
-  (print "f2 "))
+;; (defn f2 []
+;;   (print "f2 "))
 
-(ct/use-fixtures :once f1 f2)
+;; (ct/use-fixtures :once f1 f2)
 
-(defn test-ns-hook [])
+;; (defn test-ns-hook [])
