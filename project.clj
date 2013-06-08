5a6af3214437d8450d9647b1cf1fa1d113157ccb
Steven Degutis <steven@cleancoders.com>
2013-06-07 23:34:28 -0500

    1.0

--- project.clj
+++ project.clj
@@ -1,17 +1,9 @@
-(defproject test2 "0.1.0-SNAPSHOT"
+(defproject test2 "1.0.0"
   :description "Composable testing lib for Clojure"
   :url "https://github.com/evanescence/test2"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [bultitude "0.2.2"]]
-
   :codox {:src-dir-uri "https://github.com/evanescence/test2/blob/master"
-          :src-linenum-anchor-prefix "L"}
-
-  ;; :plugins [[lein-test2 "0.1.0-SNAPSHOT"]]
-  ;; this shouldnt really be in this project.
-  ;; im just testing it out.
-
-  )
+          :src-linenum-anchor-prefix "L"})
